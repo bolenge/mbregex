9a8bb7fefdbf1a4205a01a38a9be15df55a661c1
Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
2019-12-20 00:39:00 +0100

    Ajout du module isEmail() et arrangement de la documentation sur le fichier README.md

--- README.md
+++ README.md
@@ -1,28 +1,52 @@
-# Introduction
+# mbregex
 
-Librairie Javascript contenant des regex de plusieurs cas qu'on peut rencontrer (url,email,numéro telephone,...)
+Librairie Javascript contenant des regex de plusieurs cas qu'on peut rencontrer (url,email,numéro telephone, et autres...)
 
 ## Installation
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/).
 
-`npm install --save mbregex`
+```bash
+$ npm install --save mbregex
+```
 
-Example d'utilisation
+## API
 
 ```js
 var mbregex = require("mbregex");
 
 ```
 
-### isUrlValid
-Permet de vérifier si un url est valide partant d'une regex de vérification
+### mbregex.isUrlValid(url, acceptProtocol = true)
+Permet de vérifier si un url est valide
+
+#### url
+L'adresse url à vérifier
+
+#### acceptProtocol = true
+`true` par défaut s'il faut que l'url à vérifier doit contenir le protocol (http|https|ftp)
+si c'est à `false` alors on peut vérifier des urls comme `api.monsite.com` | `m.monsite.com` | `www.monsite.com` sans préfixer le protocole
+
 ```js
-var url = "https://www.npm.com"
-var url2 = "http://www.npm.com"
 
-if (mbstring.isUrlValid(url)) {
-    console.log("Adress is valid")
-}else {
-    console.log("Adress is invalid")
-}
+console.log(mbregex.isUrlValid('https://monsite.com')) // True
+console.log(mbregex.isUrlValid('https://www.monsite.com')) // True
+console.log(mbregex.isUrlValid('http://www.mon site.com')) // False
+console.log(mbregex.isUrlValid('http://mon-site.com')) // True
 
+```
+
+### mbregex.isEmail(adress)
+
+Vérifie si l'adresse passé en paramètre est une adresse Email
+
+#### adresse
+
+L'adresse à vérifier
+
+```js
+
+console.log(mbregex.isEmail('monadresseemail@site.com')) // True
+console.log(mbregex.isEmail('don028@yahoo.fr')) // True
+console.log(mbregex.isEmail('don028yahoo.fr')) // False
 ```
--- index.js
+++ index.js
@@ -3,21 +3,41 @@
  * @author Don de Dieu Bolenge
  * @version 0.1.0
  * @license ISC
- * @git ---
+ * @git https://github.com/bolenge/mbregex.git
  */
 
 /**
  * Vérifie si l'url passé en paramètre est valide
- * @param {String} url
+ * @param {String} url L'url à vérifier
+ * @param {Boolean} acceptProtocol Si on doit obliger la présence du protocol devant l'url ou pas
  * @return {Boolean}
  */
-exports.isUrlValid = (url) =>  {
-    var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-    var regex = new RegExp(expression);
+exports.isUrlValid = (url, acceptProtocol = true) =>  {
+    var oldExpression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+    var expression = /^((ftp|ftps|http|https):\/\/)?(([a-zA-Z0-9]{1,})([a-zA-Z0-9\-]{1,})?\.)?(([a-zA-Z0-9]{1,})([a-zA-Z0-9\-]{1,})?([a-zA-Z0-9]))+(\.[a-zA-Z0-9]{2,})$/i
+    var expressWithProtocol = /^((ftp|ftps|http|https):\/\/)(([a-zA-Z0-9]{1,})([a-zA-Z0-9\-]{1,})?\.)?(([a-zA-Z0-9]{1,})([a-zA-Z0-9\-]{1,})?([a-zA-Z0-9]))+(\.[a-zA-Z0-9]{2,})$/i
+    
+    var regex = acceptProtocol ? new RegExp(expressWithProtocol) : new RegExp(expression);
 
     if (regex.test(url)) {
         return true
-    }else {
-        return false
     }
+
+    return false
+}
+
+/**
+ * Vérifie si l'adresse passée en paramètre est une adresse email
+ * @param {String} adress L'adresse Email à vérifier
+ * @return {Boolean}
+ */
+exports.isEmail = (adress) => {
+    var expression = /^([a-zA-Z0-9]{1,})+(_\-)?([a-zA-Z0-9_\-]{1,})([a-zA-Z0-9]@[a-zA-Z0-9])+([a-zA-Z0-9_\-]{1,})(\.[a-zA-Z0-9]{2,})+$/i
+    var regex = new RegExp(expression);
+
+    if (regex.test(adress)) {
+        return true
+    }
+
+    return false
 }
